fe413c2086fcc6f8da60c44063e7757ade94a1e8
Valeriy Demeshko <vdemeshk@e3r9p2.unit.ua>
2017-11-02 20:21:01 +0200

    day03 ex00 header

--- day02/ex02/main.cpp
+++ day02/ex02/main.cpp
@@ -4,7 +4,7 @@
 int main(void)
 {
 	Fixed a;
-	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	Fixed const b(Fixed(23.4f) / Fixed( 0.0f ));
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
--- day03/ex00/FragTrap.hpp
+++ day03/ex00/FragTrap.hpp
@@ -9,15 +9,24 @@ class FragTrap
 {
 private:
 	int hp;
-	static const max_hp = 100;
+	static const int max_hp = 100;
 	int energy;
-	static const max_energy = 100;
+	static const int max_energy = 100;
 	int level;
-	
+	std::string name;
+	static const int hand_damage = 30;
+	static const int range_damage = 20;
+	static const int arm_reduct_damage = 5;
 public:
 	FragTrap();
+	FragTrap(FragTrap const &obj);
 	~FragTrap();
-	
+	FragTrap & operator=(FragTrap const &obj);
+	void rangedAttack(std::string const &target);
+	void maleeAttack(std::string const & target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+	void vaulthunter_dot_exe(std::string const &target);
 };
 
 #endif
